55a2969c5a14dd7abd4717b6f3b387db6a57bad0
mrbce <90007599+mrbce@users.noreply.github.com>
2021-12-02 20:43:50 +0000

    Update script.rb
    Refactored the 'if' statements to be single line

--- script.rb
+++ script.rb
@@ -1,15 +1,9 @@
 require 'prime'   # This is a module. We'll cover these soon!
 
 def first_n_primes(n)
+return "n must be an integer." unless n.is_a? Integer
+return "n must be greater than 0." if n <= 0
 
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
-
-  if n <= 0
-    return "n must be greater than 0."
-  end
-  
   return Prime.first n
 end
 
